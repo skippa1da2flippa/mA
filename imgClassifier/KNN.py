2fd6b61ab3c3ef49c0ad09d0683000061b401b68
skippa <881423@stud.unive.it>
2022-12-12 20:57:44 +0100

    texts

--- imgClassifier/KNN.py
+++ imgClassifier/KNN.py
@@ -1,3 +1,10 @@
 from fetchedData import X, y
 
-print(X)
+# represent all the 70000 vectors in an n-th dimensional space
+
+# find a way to create cluster (neighborhood)
+
+# for each asked point:
+#    place it inside the n-th dimensional space
+#    find the k-closest neighborhoods to the point
+#    assign a label to the asked point given the closest neighborhood
