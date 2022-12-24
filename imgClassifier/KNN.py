4a91deabb4f99305642d850d657b2adb27589b91
skippa <881423@stud.unive.it>
2022-12-24 01:10:13 +0100

    for fun

--- imgClassifier/KNN.py
+++ imgClassifier/KNN.py
@@ -1,14 +1,128 @@
-from fetchedData import X, y
+from math import sqrt
 
-# represent all the 70000 vectors in an n-th dimensional space
-
-# find a way to create cluster (neighborhood)
-
-# for each asked point:
-#    place it inside the n-th dimensional space
-#    find the k-closest neighborhoods to the point
-#    assign a label to the asked point given the majority vote
+nullVector: list[float] = [0.0 for i in range(0, 784)]
 
 
+def euclideanDistance(pointA: list[float], pointB: list[float]):
+    distance: float = 0.0
+    for i in range(0, 784):
+        distance += (pointA[i] - pointB[i]) ** 2
+
+    return sqrt(distance)
 
 
+class PointNdDistance:
+    def __init__(self, point: list[float], distance: float):
+        self.point = point
+        self.distance = distance
+
+
+def wrapper(comparingPoint: list[float], fromTheOrigin: bool = False):
+    def comparingWEdge(point1: PointNdDistance, point2: PointNdDistance):
+        point1Distance: float = 0.0
+        point2Distance: float = 0.0
+
+        if not fromTheOrigin:
+            point1Distance = euclideanDistance(comparingPoint, point1.point)
+            point2Distance = euclideanDistance(comparingPoint, point2.point)
+        else:
+            point1Distance = point1.distance
+            point2Distance = point2.distance
+
+        if point1Distance > point2Distance:
+            return 1
+        elif point1Distance == point2Distance:
+            return 0
+        else:
+            return -1
+
+    return comparingWEdge
+
+
+class Neighborhood:
+    # shape tells you how much margin are we taking for grabbing the right contour(the lower,the fewer points are taken)
+    shape: float = 0.7
+
+    # edge accuracy tells you how much accuracy we want to create the contour (the lower, the more precise)
+    edgeAccuracy: float = 0.3
+
+    def __init__(self, label: str):
+        self.label = label
+        self.points: list[PointNdDistance] = []
+        self.furthestPoint = []
+        self.closestPoint = []
+        self.furthestPointDistance: float = 0.0
+        self.closestPointDistance: float = 0.0
+
+    def addPoint(self, point: list[float]):
+        newDistance = euclideanDistance(nullVector, point)
+        if newDistance < self.closestPointDistance:
+            self.closestPoint = point
+            self.closestPointDistance = newDistance
+
+        if newDistance > self.furthestPointDistance:
+            self.furthestPoint = point
+            self.furthestPointDistance = newDistance
+
+        self.points.append(PointNdDistance(point, newDistance))
+
+    def __pointSorter(self):
+        self.points.sort(key=wrapper(self.closestPoint))
+
+    def findLeftNdRightBound(self, point: list[float]) -> tuple[int, int]:
+        startingLine: int = -1
+        endLine: int = -1
+        lowerDistance: float = euclideanDistance(self.closestPoint, point)
+        upperDistance: float = euclideanDistance(self.furthestPoint, point)
+        originDistance: float = euclideanDistance(nullVector, point)
+
+        if lowerDistance > upperDistance:
+            if upperDistance / lowerDistance > Neighborhood.edgeAccuracy:
+                startingLine = self.findBestStart(originDistance, False)
+                endLine = self.findBestEnd(originDistance, startingLine, False)
+            else:
+                startingLine = len(self.points) - 1
+                endLine = int(len(self.points) - (2 * (len(self.points) * Neighborhood.edgeAccuracy)))
+        else:
+            if lowerDistance / upperDistance > Neighborhood.edgeAccuracy:
+                startingLine = self.findBestStart(originDistance)
+                endLine = self.findBestEnd(originDistance, startingLine)
+            else:
+                startingLine = 0
+                endLine = int(startingLine + (2 * (len(self.points) * Neighborhood.edgeAccuracy)))
+
+        return startingLine, endLine
+
+    def findBestStart(self, distance: float, fromTheFront: bool = True):
+        if fromTheFront:
+            myRange = range(0, len(self.points))
+            for i in myRange:
+                if self.points[i].distance / distance >= Neighborhood.shape:
+                    return i
+        else:
+            myRange = range(len(self.points) - 1, 0, step=-1)
+            for i in myRange:
+                if distance / self.points[i].distance >= Neighborhood.shape:
+                    return i
+
+    def findBestEnd(self, distance: float, startingIdx: int, fromTheFront: bool = True):
+        if fromTheFront:
+            myRange = range(startingIdx, len(self.points))
+            for i in myRange:
+                if distance / self.points[i].distance <= Neighborhood.shape:
+                    return i
+        else:
+            myRange = range(startingIdx, 0, step=-1)
+            for i in myRange:
+                if self.points[i].distance / distance <= Neighborhood.shape:
+                    return i
+
+    def getTheCandidates(self, point: list[float], k: int):
+        leftRightEnd: tuple[int, int] = self.findLeftNdRightBound(point)
+        candidates: list[PointNdDistance] = []
+        for i in range(leftRightEnd[0], leftRightEnd[1] + 1):
+            candidates.append(self.points[i])
+
+        candidates.sort(key=wrapper(point))
+
+        return [candidates[i].point for i in range(len(candidates) - 1, len(candidates) - (k + 1))]
--- imgClassifier/svm.py
+++ imgClassifier/svm.py
@@ -1,6 +1,13 @@
+import os
+import time
+
+import joblib
+import pandas as pd
+import seaborn as sns
 from sklearn import svm
 from sklearn import metrics
-from sklearn.model_selection import cross_val_score, train_test_split
+from sklearn.metrics import f1_score
+from sklearn.model_selection import cross_val_score, train_test_split, GridSearchCV
 from fetchedData import X, y
 
 """
@@ -29,7 +36,6 @@ TODO you should choose by yourself which technique to apply
 (for the splitting technique) and not the default one
 """
 
-
 # create a svm Classifier
 clf = svm.SVC(kernel='linear', C=1, random_state=42)  # Linear Kernel
 
@@ -51,3 +57,160 @@ y_pred = clf.predict(X_test)
 
 # a look at the statistics
 print("accuracy: ", metrics.accuracy_score(y_test, y_pred))
+
+"""
+LINEAR RIGHT WAY 
+"""
+
+svmClf = svm.SVC(random_state=42)
+
+"""
+parameter C: For large values of C, the optimization will choose a smaller-margin hyperplane if that hyperplane does a 
+better job of getting all the training points classified correctly. Conversely, a very small value of C will cause the 
+optimizer to look for a larger-margin separating hyperplane, even if that hyperplane misclassifies more points.
+"""
+
+# split the data between training and testing data (90-10)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=109)
+
+properties = {
+    "C": [0.02, 0.12, 0.49, 0.90, 7, 20, 50, 100],
+    "kernel": ["linear"]
+}
+
+start_time = time.time()
+
+# instantiate the tuning process
+tuned_svcclsf_lin = GridSearchCV(
+    svmClf, properties, scoring="f1_weighted", cv=10, return_train_score=True, verbose=6, n_jobs=8
+)
+
+# start the tuning process
+tuned_svcclsf_lin.fit(X_train, y_train)
+print("--- %s seconds ---" % (time.time() - start_time))
+print("Best Score: {:.3f}".format(tuned_svcclsf_lin.best_score_))
+print("Best Params: ", tuned_svcclsf_lin.best_params_)
+
+# save results
+if not os.path.exists('../tuned_models'):
+    os.mkdir('../tuned_models')
+
+# to load previously saved results
+joblib.dump(tuned_svcclsf_lin, '../tuned_models/svc_linear_tuning_results.pkl')
+
+# just statistical graphs
+print(tuned_svcclsf_lin.cv_results_)
+tuned_svcclsf_lin_results = pd.DataFrame(tuned_svcclsf_lin.cv_results_)
+
+table = tuned_svcclsf_lin_results[["params", "mean_test_score", "mean_train_score"]]
+print(table)
+
+sns.set(rc={"figure.figsize": (12, 8)})
+sns.lineplot(data=tuned_svcclsf_lin_results, x="param_C", y="mean_test_score")
+
+# print the best estimator and the best score
+print(tuned_svcclsf_lin.best_estimator_)
+print(tuned_svcclsf_lin.best_score_)
+
+test_acc = f1_score(y_true=y_train,
+                    y_pred=tuned_svcclsf_lin.predict(X_train),
+                    average='weighted')
+
+print("Train F1 Score on original dataset: {}".format(test_acc))
+
+# best parameters from automatic parameters tuning
+svc_lin_clsf = svm.SVC(**tuned_svcclsf_lin.best_params_)
+
+# train the new model
+svc_lin_clsf.fit(X_train, y_train)
+
+# prediction on the training set
+svc_lin_train_pred = svc_lin_clsf.predict(X_train)
+print("Training:")
+print('F1 Score: {}'.format(f1_score(y_true=y_train, y_pred=svc_lin_train_pred, average='weighted')))
+
+# prediction on them test set
+svc_lin_test_pred = svc_lin_clsf.predict(X_test)
+print("Testing:")
+print('F1 Score: {}'.format(f1_score(y_true=y_test, y_pred=svc_lin_test_pred, average='weighted')))
+
+"""
+POLYNOMIAL OF DEGREE 2 RIGHT WAY 
+"""
+
+# automatic parameters tuning
+svcclsf_pol = svm.SVC(random_state=42)
+
+properties = {
+    "C": [0.02, 0.12, 0.49, 0.90, 7, 20, 50, 100],  # soft to hard margin
+    "kernel": ["poly"],
+    "degree": [2],
+    "gamma": ["auto", 0.1, 1]
+}
+
+# split the data between training and testing data (90-10)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=109)
+
+start_time = time.time()
+
+tuned_svcclsf_pol = GridSearchCV(
+    svcclsf_pol, properties, scoring="f1_weighted", cv=10, return_train_score=True, verbose=6, n_jobs=4
+)
+
+tuned_svcclsf_pol.fit(X_train, y_train)
+print("--- %s seconds ---" % (time.time() - start_time))
+print("Best Score: {:.3f}".format(tuned_svcclsf_pol.best_score_))
+print("Best Params: ", tuned_svcclsf_pol.best_params_)
+
+# save results
+if not os.path.exists('../tuned_models'):
+    os.mkdir('../tuned_models')
+
+joblib.dump(tuned_svcclsf_pol, '../tuned_models/svc_polynomial_tuning_results.pkl')
+
+# to load previously saved results
+tuned_svcclsf_pol = joblib.load("../tuned_models/svc_polynomial_tuning_results.pkl")
+
+# just printing some graphs
+print(tuned_svcclsf_pol.cv_results_)
+tuned_svcclsf_pol_results = pd.DataFrame(tuned_svcclsf_pol.cv_results_)
+
+table = tuned_svcclsf_pol_results[["params", "mean_test_score", "mean_train_score"]]
+print(table)
+
+sns.set(rc={"figure.figsize": (12, 8)})
+sns.lineplot(data=tuned_svcclsf_pol_results, x="param_C", y="mean_test_score")
+
+# print the best estimator and the best score
+print(tuned_svcclsf_pol.best_estimator_)
+print(tuned_svcclsf_pol.best_score_)
+
+test_acc = f1_score(y_true=y_train,
+                    y_pred=tuned_svcclsf_pol.predict(X_train),
+                    average='weighted')
+print("Train F1 Score on original dataset: {}".format(test_acc))
+
+# best parameters from automatic parameters tuning
+svc_pol_clsf = svm.SVC(**tuned_svcclsf_pol.best_params_)
+
+# train the new model
+svc_pol_clsf.fit(X_train, y_train)
+
+# check the accuracy for the training dataset
+svc_pol_train_pred = svc_pol_clsf.predict(X_train)
+print("Training:")
+print('F1 Score: {}'.format(f1_score(y_true=y_train,
+                                     y_pred=svc_pol_train_pred,
+                                     average='weighted')))
+
+# check the accuracy for the testing dataset
+svc_pol_test_pred = svc_pol_clsf.predict(X_test)
+print("Testing:")
+print('F1 Score: {}'.format(f1_score(y_true=y_test,
+                                     y_pred=svc_pol_test_pred,
+                                     average='weighted')))
+
+
+"""
+RBF KERNEL RIGHT WAY
+"""
