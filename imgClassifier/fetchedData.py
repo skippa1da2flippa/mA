c4d6afe249ddfe1a2bd9e04769d95cfe65c0873f
skippa <881423@stud.unive.it>
2022-12-26 14:11:27 +0100

    okme

--- imgClassifier/fetchedData.py
+++ imgClassifier/fetchedData.py
@@ -6,5 +6,5 @@ y = y.astype(int)
 X = X / 255
 
 # split the data between training and testing data (90-10)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.95, random_state=109)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.99, random_state=109)
 
